03dca08187d1ce64f14496429bc886e5f2cbdcbc
thitoribeiro <thito.ribeiro@hotmail.com>
2021-12-04 09:56:50 -0300

    Commiting all files reviewed

---
+++ src/test/scala/gatlingdemostore/DemostoreSimulation.scala
@@ -0,0 +1,76 @@
+package gatlingdemostore
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class DemostoreSimulation extends Simulation {
+
+	val domain = "demostore.gatling.io"
+
+	val httpProtocol = http
+		.baseUrl("http://" + domain)
+
+	val categoryFeeder = csv("data/categoryDetails.csv").random
+
+	object CmsPages {
+		def homepage = {
+			exec(http("Load Home Page")
+				.get("/")
+				.check(status.is(200))
+				.check(regex("<title>Gatling Demo-Store</title>").exists)
+				.check(css("#_csrf", "content").saveAs("csrfValue")))
+		}
+
+		def aboutUs = {
+			exec(http("Load About Us Page")
+				.get("/about-us")
+				.check(status.is(200))
+				.check(substring("About Us"))
+			)
+		}
+	}
+
+	object Catalog {
+		object Category {
+			def view = {
+				feed(categoryFeeder)
+					.exec(http("Load Category Page - ${categoryName}")
+						.get("/category/${categorySlug}")
+						.check(status.is(200))
+						.check(css("#CategoryName").is("${categoryName}"))
+					)
+
+			}
+		}
+	}
+
+		val scn = scenario("DemostoreSimulation")
+			.exec(CmsPages.homepage)
+			.pause(2)
+			.exec(CmsPages.aboutUs)
+			.pause(2)
+			.exec(Catalog.Category.view)
+
+		.pause(2)
+		.exec(http("Load Product Page")
+			.get("/product/black-and-red-glasses"))
+		.pause(2)
+		.exec(http("Add Product to Cart")
+			.get("/cart/add/19"))
+		.pause(2)
+		.exec(http("View Cart")
+			.get("/cart/view"))
+		.pause(2)
+		.exec(http("Login User")
+			.post("/login")
+			.formParam("_csrf", "${csrfValue}")
+			.formParam("username", "user1")
+			.formParam("password", "pass"))
+		.pause(2)
+		.exec(http("Load Checkout Information")
+			.get("/cart/checkoutConfirmation")
+			.resources(http("Checkout")
+			.get("/cart/checkout")))
+
+	setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
+}
